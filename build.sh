2f3525508a7f8d954f48784ce5e3f94a4ac0c8ad
アンドレイ <xtensei.is.not.in.the.sudoers.file@protonmail.com>
2026-06-16 16:45:55 +0800

    branch for bbrv3 and build.sh

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ export ARCH=arm64
 export LLVM=1
 export LLVM_IAS=1
 export KBUILD_BUILD_USER="GrayRavens-Team"
-export KBUILD_BUILD_HOST="Zenithed-MGLRU-Eng"
+export KBUILD_BUILD_HOST="Zenithed-BBRV3-Eng"
 
 # ── Clang toolchain ──────────────────────────────────────────────────────────
 if [ -z "$CLANG_PATH" ]; then
